ceb0aca052d3a698168f8846cd9698b02aa39182
Kirill Drozdov <kirill.dzv@gmail.com>
2022-09-25 20:51:02 +0200

    Refactored layout

---
+++ handlers/handler.go
@@ -0,0 +1,188 @@
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"recipes-api/models"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type RecipesHandler struct {
+	collection *mongo.Collection
+	ctx        context.Context
+}
+
+func NewRecipesHandler(ctx context.Context, collection *mongo.Collection) *RecipesHandler {
+	return &RecipesHandler{
+		collection: collection,
+		ctx:        ctx,
+	}
+}
+
+// swagger:operation POST /recipes recipes createRecipe
+// Create a new recipe
+// ---
+// produces:
+// - application/json
+// responses:
+//   '200':
+// 	   description: Successful operation
+//   '400':
+//	   description: Invalid input
+func (handler *RecipesHandler) NewRecipe(c *gin.Context) {
+	var recipe models.Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	recipe.ID = primitive.NewObjectID()
+	recipe.PublishedAt = time.Now()
+	_, err := handler.collection.InsertOne(handler.ctx, recipe)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while inserting a new recipe"})
+		return
+	}
+	c.JSON(http.StatusOK, recipe)
+}
+
+// swagger:operation GET /recipes recipes listRecipes
+// Returns list of recipes
+// ---
+// produces:
+// - application/json
+// responses:
+//   '200':
+//	   description: Successful operation
+func (handler *RecipesHandler) ListRecipes(c *gin.Context) {
+	cur, err := handler.collection.Find(handler.ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer cur.Close(handler.ctx)
+
+	recipes := make([]models.Recipe, 0)
+	for cur.Next(handler.ctx) {
+		var recipe models.Recipe
+		cur.Decode(&recipe)
+		recipes = append(recipes, recipe)
+	}
+
+	c.JSON(http.StatusOK, recipes)
+}
+
+// swagger:operation PUT /recipes/:id recipes updateRecipe
+// Update an existing recipe
+// ---
+// parameters:
+// - name: id
+//   in: query
+//   description: ID of the recipe
+//   required: true
+//   type: string
+// produces:
+// - application/json
+// responses:
+//   '200':
+// 	   description: Successful operation
+//   '400':
+//	   description: Invalid input
+//   '404':
+//	   description: Invalid recipe ID
+func (handler *RecipesHandler) UpdateRecipe(c *gin.Context) {
+	id := c.Param("id")
+	var recipe models.Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	_, err := handler.collection.UpdateOne(handler.ctx, bson.M{"_id": objectId}, bson.D{
+		{Key: "$set", Value: bson.D{
+			{Key: "name", Value: recipe.Name},
+			{Key: "instructions", Value: recipe.Instructions},
+			{Key: "ingredients", Value: recipe.Ingredients},
+			{Key: "tags", Value: recipe.Tags},
+		}}})
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Recipe has been updated"})
+}
+
+// swagger:operation DELETE /recipes/:id recipes deleteRecipe
+// Delete an existing recipe
+// ---
+// parameters:
+// - name: id
+//   in: query
+//   description: ID of the recipe
+//   required: true
+//   type: string
+// produces:
+// - application/json
+// responses:
+//   '200':
+// 	   description: Successful operation
+//   '404':
+//	   description: Invalid recipe ID
+func (handler *RecipesHandler) DeleteRecipe(c *gin.Context) {
+	id := c.Param("id")
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	_, err := handler.collection.DeleteOne(handler.ctx, bson.M{"_id": objectId})
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"error": "Recipe has been deleted"})
+}
+
+// swagger:operation GET /recipes/search recipes searchRecipe
+// Search recipes by tag
+// ---
+// parameters:
+// - name: tag
+//   in: query
+//   description: tag to search by
+//   required: true
+//   type: string
+// produces:
+// - application/json
+// responses:
+//   '200':
+// 	   description: Successful operation
+func (handler *RecipesHandler) SearchRecipes(c *gin.Context) {
+	tag := c.Query("tag")
+
+	filter := bson.D{{Key: "tags", Value: bson.D{{
+		Key:   "$in",
+		Value: bson.A{tag},
+	}}}}
+
+	cur, err := handler.collection.Find(handler.ctx, filter)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer cur.Close(handler.ctx)
+
+	recipes := make([]models.Recipe, 0)
+	for cur.Next(handler.ctx) {
+		var recipe models.Recipe
+		cur.Decode(&recipe)
+		recipes = append(recipes, recipe)
+	}
+
+	c.JSON(http.StatusOK, recipes)
+}
